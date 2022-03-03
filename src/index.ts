85c371b7f82b214d97eee4cb4aff3e384041df36
Josh Merlino <joshua.merlino@gmail.com>
2022-03-02 21:11:57 -0500

    bruh

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 // Configure variables in environment file (.env)
+import { randomBytes } from "crypto";
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 dotenv.config();
+
+(async function main() {
+	const time = Date.now();
+	try {
+		fetch(process.env.ENDPOINT!)
+			.then(resp => resp.text())
+			.then(() => {
+				console.log(`Time taken: ${Date.now() - time}ms`, randomBytes(4).toString("hex"));
+				main();
+			});
+	} catch (error) {
+		setImmediate(main);
+	}
+}());
